550179a58d013dc9fc46f83ae44e57d6b0f471c0
Porty-Eric <ericlesdsantos@gmail.com>
2021-05-07 22:00:51 -0300

    Fim do dia 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import { Header } from '../components/Header'
-
 import '../styles/global.scss'
 
+import { Header } from '../components/Header'
+import {Player} from '../components/Player';
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className= {styles.appWrapper}>
+    <div className= {styles.wrapper}>
       <main>
         <Header />
         <Component {...pageProps} />
       </main>
+      <Player />
     </div>
   )
 }
